300e6febfaf6424ee58d786087b2ca6ab46afa3a
TakuKobayshi <>
2018-06-04 12:25:10 +0900

    とりあえずテーブルを作ってみた

--- cca/adnem/models.py
+++ cca/adnem/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class User(models.Model):
+    uid = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    last_login_at = models.DateTimeField(null=False, blank=False)
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    type = models.CharField(max_length=32)
+    uid = models.CharField(max_length=255, null=False, blank=False)
+    token = models.TextField(null=True, blank=True)
+    token_secret = models.TextField(null=True, blank=True)
+    expired_at = models.DateTimeField(null=True, blank=True)
+    options = models.TextField(null=True, blank=True)
+
 # Create your models here.
--- cca/cca/settings.py
+++ cca/cca/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'adnem.apps.AdnemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
