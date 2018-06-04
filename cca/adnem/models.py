b769c557f10e2b84fafde40c186bbac3bab027fe
TakuKobayshi <>
2018-06-04 14:04:20 +0900

    テーブル構造変更

--- cca/adnem/models.py
+++ cca/adnem/models.py
@@ -8,6 +8,14 @@ class User(models.Model):
     updated_at = models.DateTimeField(auto_now=True, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
+class Publisher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    host = models.CharField(max_length=255, null=False, blank=False)
+    url = models.TextField(null=False, blank=False)
+    activate_state = models.IntegerField(null=False, blank=False, default=0)
+    updated_at = models.DateTimeField(auto_now=True, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+
 class Account(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     type = models.CharField(max_length=32, null=True, blank=True)
@@ -20,7 +28,7 @@ class Account(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
 class Wallet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     currency_prefix = models.CharField(max_length=32, null=False, blank=False)
     currency_name = models.CharField(max_length=32, null=False, blank=False)
     address = models.CharField(max_length=255, null=False, blank=False)
@@ -30,10 +38,10 @@ class Wallet(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
 class Advertisement(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=False, blank=False)
+    investee_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    investee_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, null=False, blank=False)
     uuid = models.CharField(max_length=255, null=False, blank=False)
-    host = models.CharField(max_length=255, null=False, blank=False)
-    url = models.TextField(null=False, blank=False)
     daily_price = models.FloatField(null=False, blank=False, default=0)
     sum_price = models.FloatField(null=False, blank=False, default=0)
     min_price = models.FloatField(null=False, blank=False, default=0)
@@ -50,7 +58,7 @@ class Advertisement(models.Model):
 
 class AdvertisementLog(models.Model):
     advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE, null=False, blank=False)
-    host = models.CharField(max_length=255, null=False, blank=False)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=False, blank=False)
     accessed_by_ipaddress = models.CharField(max_length=255, null=False, blank=False)
     accessed_by_user_token = models.CharField(max_length=255, null=False, blank=False)
     accessed_by_user_agent = models.TextField(null=True, blank=True)
